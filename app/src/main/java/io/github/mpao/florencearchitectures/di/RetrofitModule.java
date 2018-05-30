d49b089afff4aff308675195618d56de00c69764
marco <mpao@me.com>
2018-05-30 09:53:52 +0200

    fix endpoint encoding

--- app/src/main/java/io/github/mpao/florencearchitectures/di/RetrofitModule.java
+++ app/src/main/java/io/github/mpao/florencearchitectures/di/RetrofitModule.java
@@ -2,12 +2,18 @@ package io.github.mpao.florencearchitectures.di;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.io.IOException;
 import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import io.github.mpao.florencearchitectures.entities.Building;
 import io.github.mpao.florencearchitectures.entities.BuildingDeserializer;
 import io.github.mpao.florencearchitectures.models.networks.OpenDataApi;
+import okhttp3.Interceptor;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -28,10 +34,37 @@ public class RetrofitModule {
 
     private Retrofit getRetrofit(Gson gson){
 
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new ResponseInterceptor())
+                .build();
+
         return new Retrofit.Builder()
                 .baseUrl(FLORENCE_OPENDATA)
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
                 .build();
+
+    }
+
+    /*
+     * Endpoint has a wrong encoding text
+     * @see https://stackoverflow.com/q/45284974/1588252
+     */
+    private class ResponseInterceptor implements Interceptor{
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Response response = chain.proceed(chain.request());
+            MediaType m       = MediaType.parse("application/json; charset=windows-1252");
+            ResponseBody body = response.body();
+            if(body != null) {
+                ResponseBody r = ResponseBody.create(m, body.bytes());
+                return response.newBuilder()
+                        .body(r)
+                        .build();
+            }
+            return response;
+        }
 
     }
 
