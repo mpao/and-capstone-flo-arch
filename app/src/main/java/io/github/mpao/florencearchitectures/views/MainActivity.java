8bd158ba8e8c7517496cf1b73a5e8dcde9ecbd22
marco <mpao@me.com>
2018-06-01 22:23:32 +0200

    add splashscreen, icon, images placeholder

--- app/src/main/java/io/github/mpao/florencearchitectures/views/MainActivity.java
+++ app/src/main/java/io/github/mpao/florencearchitectures/views/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        setTheme(R.style.AppTheme); // see splash_screen.xml
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
