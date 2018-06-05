b4ee372532f1440e50048884d0fd631f61e4cbb4
marco <mpao@me.com>
2018-06-05 15:54:48 +0200

    implement detail activity

---
+++ app/src/main/java/io/github/mpao/florencearchitectures/views/adapters/ImagesAdapter.java
@@ -0,0 +1,63 @@
+package io.github.mpao.florencearchitectures.views.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+import java.util.List;
+import io.github.mpao.florencearchitectures.R;
+
+public class ImagesAdapter extends RecyclerView.Adapter<ImagesAdapter.ViewHolder>{
+
+    private Context context;
+    private List<String> list;
+
+    public ImagesAdapter(Context context, List<String> list){
+        this.context = context;
+        this.list    = list;
+    }
+
+    @Override
+    @NonNull
+    public ImagesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        this.context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View row = inflater.inflate(R.layout.content_building_image_row, parent, false);
+        return new ViewHolder(row);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImagesAdapter.ViewHolder viewHolder, int position) {
+
+        Picasso.get().load(list.get(position)).into(viewHolder.imageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return list.size();
+
+    }
+
+    /*
+    ViewHolder class
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+
+        private ViewHolder(View view){
+            super(view);
+            imageView = view.findViewById(R.id.preview);
+        }
+
+    }
+
+}
